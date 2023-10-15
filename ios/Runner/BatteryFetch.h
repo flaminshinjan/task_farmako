837e142e6c583ce8ad6df12b1c9e285254d06989
flaminshinjan <srikpatra.2008@gmail.com>
2023-10-15 23:57:34 +0530

    hehe

---
+++ ios/Runner/BatteryFetch.h
@@ -0,0 +1,12 @@
+//
+//  BatteryFetch.h
+//  Runner
+//
+//  Created by Shinjan Patra on 15/10/23.
+//
+
+#ifndef BatteryFetch_h
+#define BatteryFetch_h
+
+
+#endif /* BatteryFetch_h */
---
+++ ios/Runner/BatteryFetcher.m
@@ -0,0 +1,8 @@
+//
+//  BatteryFetcher.m
+//  Runner
+//
+//  Created by Shinjan Patra on 15/10/23.
+//
+
+#import <Foundation/Foundation.h>
