3b33738fa654bad9798d31279db273b5a50a36fa
flaminshinjan <srikpatra.2008@gmail.com>
2023-10-16 17:03:45 +0530

    finally done w it

---
+++ ios/Runner/BatteryPlugin.h
@@ -0,0 +1,12 @@
+//
+//  BatteryPlugin.h
+//  Runner
+//
+//  Created by Shinjan Patra on 16/10/23.
+//
+
+#ifndef BatteryPlugin_h
+#define BatteryPlugin_h
+
+
+#endif /* BatteryPlugin_h */
---
+++ ios/Runner/BatteryPlugin.m
@@ -0,0 +1,8 @@
+//
+//  BatteryPlugin.m
+//  Runner
+//
+//  Created by Shinjan Patra on 16/10/23.
+//
+
+#import <Foundation/Foundation.h>
